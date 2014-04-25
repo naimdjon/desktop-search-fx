68579eda583551ecbe355e97edf69c154a080fc8
naimdjon <naimdjon@gmail.com>
2014-04-25 12:15:35 +0200

    build script, make tests work.

--- src/test/java/org/desktopsearch/IndexTest.java
+++ src/test/java/org/desktopsearch/IndexTest.java
@@ -14,7 +14,7 @@ import java.nio.file.StandardOpenOption;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-public class IndexTests {
+public class IndexTest {
     final static String INDEX_PATH = System.getProperty("user.dir").concat(File.separator).concat("index");
     private File index;
 
