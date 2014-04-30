095f25bc4d794312229619067e0ccff18ab2eb26
Naimdjon <naimdjon@gmail.com>
2014-04-30 20:19:57 +0200

    show nr. of hits

--- src/main/java/org/desktopsearch/MainController.java
+++ src/main/java/org/desktopsearch/MainController.java
@@ -7,9 +7,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.apache.lucene.search.TopDocs;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import static org.desktopsearch.search.LocalSearcher.search;
 
 public class MainController implements Initializable {
     @FXML
@@ -28,7 +31,14 @@ public class MainController implements Initializable {
     }
 
     private void performSearch(final ActionEvent event) {
-
+        try {
+            final TopDocs result = search(queryTextField.getText());
+            System.out.printf("result.totalHits:" + result.totalHits);
+            totalHits.setText("Total hits:" + result.totalHits);
+        } catch (Exception e) {
+            e.printStackTrace();
+            new AlertDialog(String.format("Error searching (%s)!",e.getMessage())).show(stage);
+        }
     }
 
     public void setStage(Stage stage) {
