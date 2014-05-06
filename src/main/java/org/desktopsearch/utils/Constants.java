e684ac294844396887c357a903c06cead155485c
Naimdjon <naimdjon@gmail.com>
2014-05-06 22:36:59 +0200

    use enum for fields.

--- src/main/java/org/desktopsearch/utils/Constants.java
+++ src/main/java/org/desktopsearch/utils/Constants.java
@@ -5,4 +5,9 @@ import java.io.File;
 public class Constants {
     public static final String DEFAULT_INDEX_PATH = System.getProperty("user.dir").concat(File.separator).concat("index");
     public static final String INDEX_PATH = System.getProperty("desktopsearch.index.path", DEFAULT_INDEX_PATH);
+
+    public enum Fields{
+        contents
+    }
+
 }
