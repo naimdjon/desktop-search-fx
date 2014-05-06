a8c19ede3bc959bf6797e60fe1f9cc3837160270
Naimdjon <naimdjon@gmail.com>
2014-05-06 21:54:37 +0200

    use fxml for dispatching events.

--- src/main/java/org/desktopsearch/SearchController.java
+++ src/main/java/org/desktopsearch/SearchController.java
@@ -2,22 +2,14 @@ package org.desktopsearch;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.apache.lucene.search.TopDocs;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import static org.desktopsearch.search.LocalSearcher.search;
 
-public class SearchController implements Initializable {
-    @FXML
-    private Button searchButton;
-
+public class SearchController {
     @FXML
     private TextField queryTextField;
 
@@ -26,13 +18,8 @@ public class SearchController implements Initializable {
 
     private Stage stage;
 
-    @Override
-    public void initialize(final URL location, final ResourceBundle resources) {
-        searchButton.setOnAction(this::performSearch);
-        queryTextField.setOnAction(this::performSearch);
-    }
-
-    private void performSearch(final ActionEvent event) {
+    @SuppressWarnings("unused")
+    public void performSearch(final ActionEvent event) {
         try {
             final TopDocs result = search(queryTextField.getText());
             System.out.println("result.totalHits:" + result.totalHits);
