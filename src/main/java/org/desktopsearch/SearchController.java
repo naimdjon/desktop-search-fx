37348e90cb2465ce6c88acbc465d07b7db4c1f78
Naimdjon <naimdjon@gmail.com>
2014-05-06 22:38:20 +0200

    searching with hit processor.

--- src/main/java/org/desktopsearch/SearchController.java
+++ src/main/java/org/desktopsearch/SearchController.java
@@ -2,28 +2,40 @@ package org.desktopsearch;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import org.apache.lucene.search.TopDocs;
+
+import java.util.Collection;
+import java.util.LinkedHashSet;
 
 import static org.desktopsearch.search.LocalSearcher.search;
+import static org.desktopsearch.utils.Constants.Fields.contents;
 
 public class SearchController {
     @FXML
     private TextField queryTextField;
 
     @FXML
-    private Label totalHits;
+    private Label totalHitsLabel;
+
+    @FXML
+    private VBox searchResults;
 
     private Stage stage;
 
     @SuppressWarnings("unused")
     public void performSearch(final ActionEvent event) {
         try {
-            final TopDocs result = search(queryTextField.getText());
-            System.out.println("result.totalHits:" + result.totalHits);
-            totalHits.setText("Total hits:" + result.totalHits);
+            final Collection<Node> children= new LinkedHashSet<>();
+            final int totalHits = search(queryTextField.getText(), doc -> {
+                children.add(new Label(doc.get(contents.name())));
+            });
+            totalHitsLabel.setText("Total hits:" + totalHits);
+            searchResults.getChildren().addAll(children);
+            System.out.println("result.totalHitsLabel:" + totalHits);
         } catch (Exception e) {
             e.printStackTrace();
             new AlertDialog(String.format("Error searching (%s)!",e.getMessage())).show(stage);
