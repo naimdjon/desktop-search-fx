4dddb24f359aedb5c81ce49a5afecfd7e410a479
naimdjon <naimdjon@gmail.com>
2014-05-15 20:44:01 +0200

    Missing return...

--- src/main/java/org/desktopsearch/SearchController.java
+++ src/main/java/org/desktopsearch/SearchController.java
@@ -37,6 +37,7 @@ public class SearchController {
         try {
             if (Strings.isEmpty(queryTextField.getText())) {
                 MessageDialog.error("Empty query", stage);
+                return;
             }
             final Collection<String> children = new LinkedHashSet<>();
             search(queryTextField.getText(), System.out::println);
